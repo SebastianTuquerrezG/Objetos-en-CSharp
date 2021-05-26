c115e5737fcced43e223e075e11b03766144b219
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-05-25 21:24:22 -0500

    Se agregaron las operaciones accesoras, mutadoras y los constructores

--- libAlcancia/libAlcancia/Class1.cs
+++ libAlcancia/libAlcancia/Class1.cs
@@ -20,5 +20,82 @@ namespace Alcancia
         private List<int> atrConteoPorDenominacionBilletes = null;
         private int atrSaldoTotalBilletes = 0;
         private int atrSaldoTotal = 0;
+
+        public string darNombre()
+        {
+            return atrNombre;
+        }
+        public int darCapacidadMonedas()
+        {
+            return atrCapacidadMonedas;
+        }
+        public int darCapacidadBilletes()
+        {
+            return atrCapacidadBilletes;
+        }
+        public List<int> darDenominacionesAceptadasMonedas()
+        {
+            return atrDenominacionesAceptadasMonedas;
+        }
+        public List<int> darDenominacionesAceptadasBilletes()
+        {
+            return atrDenominacionesAceptadasBilletes;
+        }
+        public List<int> darSaldoPorDenominacionMonedas()
+        {
+            return atrSaldoPorDenominacionMonedas;
+        }
+        public List<int> darSaldoPorDenominacionBilletes()
+        {
+            return atrSaldoPorDenominacionBilletes;
+        }
+        public List<int> darConteoPorDenominacionMonedas()
+        {
+            return atrConteoPorDenominacionMonedas;
+        }
+        public List<int> darConteoPorDenominacionBilletes()
+        {
+            return atrConteoPorDenominacionBilletes;
+        }
+        public int darSaldoTotalMonedas()
+        {
+            return atrSaldoTotalMonedas;
+        }
+        public int darSaldoTotalBilletes()
+        {
+            return atrSaldoTotalBilletes;
+        }
+        public void ponerNombre(string prmValor)
+        {
+            atrNombre = prmValor;
+        }
+        public void ponerCapacidadMonedas(int prmValor)
+        {
+            atrCapacidadMonedas = prmValor;
+        }
+        public void ponerCapacidadBilletes(int prmValor)
+        {
+            atrCapacidadBilletes = prmValor;
+        }
+        public void ponerDenominacionesAceptadasMonedas(List<int> prmLista)
+        {
+            atrDenominacionesAceptadasMonedas = prmLista;
+        }
+        public void ponerDenominacionesAceptadasBilletes(List<int> prmLista)
+        {
+            atrDenominacionesAceptadasBilletes = prmLista;
+        }
+
+        public clsALCANCIA()
+        {
+        }
+        public clsALCANCIA(string prmNombre, int prmCapacidadMonedas, List<int> prmDenominacionesAceptadasMonedas, int prmCapacidadBilletes, List<int> prmDenominacionesAceptadasBilletes)
+        {
+            atrNombre = prmNombre;
+            atrCapacidadMonedas = prmCapacidadMonedas;
+            atrDenominacionesAceptadasMonedas = prmDenominacionesAceptadasMonedas;
+            atrCapacidadBilletes = prmCapacidadBilletes;
+            atrDenominacionesAceptadasBilletes = prmDenominacionesAceptadasBilletes;
+        }
     }
 }
