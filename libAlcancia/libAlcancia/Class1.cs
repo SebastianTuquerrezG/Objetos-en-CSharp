608833590ceb109a5a2594268ef6fbbd82281ce0
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-05-25 21:17:54 -0500

    Se agregaron los atributos propios y derivables

--- libAlcancia/libAlcancia/Class1.cs
+++ libAlcancia/libAlcancia/Class1.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+// File:    clsALCANCIA.cs
+// Author:  JOAN SEBASTIAN TUQUERREZ GOMEZ
+// Created: martes, 25 de mayo de 2021 4:57:43 p. m.
+// Purpose: Definition of Class clsALCANCIA
 
-namespace libAlcancia
+namespace Alcancia
 {
-    public class Class1
+    public class clsALCANCIA
     {
+        private string atrNombre = "Ninguno";
+        private int atrCapacidadMonedas = 0;
+        private List<int> atrDenominacionesAceptadasMonedas = null;
+        private int atrCapacidadBilletes = 0;
+        private List<int> atrDenominacionesAceptadasBilletes = null;
+        private List<int> atrSaldoPorDenominacionMonedas = null;
+        private List<int> atrConteoPorDenominacionMonedas = null;
+        private int atrSaldoTotalMonedas = 0;
+        private List<int> atrSaldoPorDenominacionBilletes = null;
+        private List<int> atrConteoPorDenominacionBilletes = null;
+        private int atrSaldoTotalBilletes = 0;
+        private int atrSaldoTotal = 0;
     }
 }
